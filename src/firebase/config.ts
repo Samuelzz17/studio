9c7e408f4d36bcfd7dd2c0ac4391b8f2477bba44
I Gede Eka Samdyana Putra <MzAzu6@gmail.com>
2026-01-26 14:30:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2575278413-19a95",
+  "appId": "1:213542699769:web:811479893b521409c11828",
+  "apiKey": "AIzaSyCK3qr9egXaeHPkPMwyZsu54giyrN8qj10",
+  "authDomain": "studio-2575278413-19a95.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "213542699769"
+};
